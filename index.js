002880ee177bf1d4026c6592ab458f41141207ef
jeovan <jeovan.delavechea88@gmail.com>
2023-11-03 09:39:39 -0300

    adicionado arquivo com operações matemáticas no terminal usando JavaScript

---
+++ index.js
@@ -0,0 +1,8 @@
+let operacoesMatematicas = (num1, num2) => {
+    console.log(`${num1} * ${num2} = ${num1 * num2}`);
+    console.log(`${num1} / ${num2} = ${num1 / num2}`);
+    console.log(`${num1} + ${num2} = ${num1 + num2}`);
+    console.log(`${num1} - ${num2} = ${num1 - num2}`);
+}
+
+operacoesMatematicas(3, 9)
